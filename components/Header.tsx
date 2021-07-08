10b56432f0e4ffb8fdc9408e218a2c9dffbb9d43
bernaRocha <mrochabernardo@gmail.com>
2021-07-08 13:26:21 -0300

    Calculadora de IMC

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import styles from '../styles/header.module.scss'
+import { Container, Grid, Typography } from '@material-ui/core'
+
+export default function Header() {
+    return(
+        <div className={styles.header}>
+            <Container fixed>
+                {/* LINHA */}
+                <Grid container justify='center' alignItems='center' spacing={5}>
+                    {/* COLUNA */}
+                    <Grid item xs={12} sm={6}>
+                        <Typography variant='h3' className={styles.header_title}>Calcule o seu IMC</Typography>
+
+                        <Typography>O IMC é o Índice de Massa Corporal e é utilizado para saber se o peso
+                            está de acordo com a Altura.
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                        <img src='/images/imagem.png' alt='Imagem de um médico avaiando um paciente' />
+
+                    </Grid>
+                </Grid>
+            </Container>
+
+            
+
+        </div>
+
+    )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+import Calculator from '../components/Calculator'
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Calculator />
+    </>
+  )
+}
